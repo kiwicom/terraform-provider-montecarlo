72aaa3124f2b29ee9c080fc97176f37b669b577a
ndopj <ndopjera@gmail.com>
2024-06-30 00:48:59 +0200

    fix(domain|test): update - at least one 'assignment' or 'tag' must be always set

--- internal/testdata/TestAccDomainResource/update_assignments.tf
+++ internal/testdata/TestAccDomainResource/update_assignments.tf
@@ -17,7 +17,7 @@ resource "montecarlo_domain" "test" {
   name        = "domain2"
   description = "Domain test description 2"
   assignments = [
-    #"MCON++a84380ed-b962-4bd3-b150-04bc38a209d5++427a1600-2653-40c5-a1e7-5ec98703ee9d++project++gcp-project1-722af1c6",
-    #"MCON++a84380ed-b962-4bd3-b150-04bc38a209d5++e7c59fd6-7ca8-41e7-8325-062ea38d3df5++dataset++postgre-dataset-1"
+    "MCON++3e9abc75-5dc1-447e-b4cb-9d5a6fc5db5c++da6c0716-2724-4bfc-b5cc-7e0364faf979++table++data-playground-8bb9fc23:terraform_provider_montecarlo.person",
+    "MCON++3e9abc75-5dc1-447e-b4cb-9d5a6fc5db5c++da6c0716-2724-4bfc-b5cc-7e0364faf979++table++data-playground-8bb9fc23:terraform_provider_montecarlo.device"
   ]
 }
